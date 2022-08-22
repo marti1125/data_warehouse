07c4cacf9fc4f4d73e5ccedfa42568c4215c2932
Willy Aguirre <marti1125@gmail.com>
2022-08-21 22:19:36 -0300

    fixed queries

--- sql_queries.py
+++ sql_queries.py
@@ -22,23 +22,22 @@ staging_events_table_create= ("""
         staging_events_id INT IDENTITY(1, 1),
         artist VARCHAR(255),
         auth VARCHAR(50),
-        first_name VARCHAR(255),
+        firstName VARCHAR(255),
         gender CHAR(1),
-        item_in_session INT,
-        last_name VARCHAR(255),
+        itemInSession INT,
+        lastName VARCHAR(255),
         length DOUBLE PRECISION,
         level CHAR(10),
         location VARCHAR(255),
         method CHAR(3),
         page VARCHAR(50),
         registration DOUBLE PRECISION,
-        session_id BIGINT,
+        sessionId BIGINT,
         song VARCHAR(255),
         status INT,
         ts BIGINT,
-        user_agent TEXT,
-        user_id BIGINT,
-        PRIMARY KEY (staging_events_id)
+        userAgent TEXT,
+        userId BIGINT
     );
 """)
 
@@ -54,8 +53,7 @@ staging_songs_table_create = ("""
         num_songs INT,
         song_id VARCHAR(255),
         title VARCHAR(255),
-        year INT,
-        PRIMARY KEY (staging_songs_id)
+        year INT
     );
 """)
 
@@ -77,7 +75,7 @@ songplay_table_create = ("""
 user_table_create = ("""
     CREATE TABLE IF NOT EXISTS users(
         user_id INT,
-        first_name VARCHAR(255),
+        first_name VARCHAR(255) not null,
         last_name VARCHAR(255),
         gender CHAR(1),
         level CHAR(10),
@@ -88,8 +86,8 @@ user_table_create = ("""
 song_table_create = ("""
     CREATE TABLE IF NOT EXISTS songs(
         song_id VARCHAR(255),
-        title VARCHAR(255),
-        artist_id VARCHAR(255),
+        title VARCHAR(255) not null,
+        artist_id VARCHAR(255) not null,
         year INT,
         duration DOUBLE PRECISION,
         PRIMARY KEY (song_id)
@@ -98,8 +96,8 @@ song_table_create = ("""
 
 artist_table_create = ("""
     CREATE TABLE IF NOT EXISTS artists(
-        artist_id INT,
-        name VARCHAR(255),
+        artist_id VARCHAR(255),
+        name VARCHAR(255) not null,
         location VARCHAR(255),
         lattitude DOUBLE PRECISION,
         longitude DOUBLE PRECISION,
@@ -123,31 +121,31 @@ time_table_create = ("""
 # STAGING TABLES
 
 staging_events_copy = ("""
-    copy staging_events from 's3://udacity-dend/log_data'
+    copy staging_events from {}
     credentials 'aws_iam_role={}'
     COMPUPDATE OFF
-    json 's3://udacity-dend/log_json_path.json' region 'us-west-2';
-""").format(config['IAM_ROLE']['ARN'])
+    json {} region 'us-west-2';
+""").format(config['S3']['LOG_DATA'], config['IAM_ROLE']['ARN'], config['S3']['LOG_JSONPATH'])
 
 staging_songs_copy = ("""
-    copy staging_songs from 's3://udacity-dend/song_data'
+    copy staging_songs from {}
     credentials 'aws_iam_role={}'
     COMPUPDATE OFF
     json 'auto' region 'us-west-2';
-""").format(config['IAM_ROLE']['ARN'])
+""").format(config['S3']['SONG_DATA'], config['IAM_ROLE']['ARN'])
 
 # FINAL TABLES
 
 songplay_table_insert = ("""
     INSERT INTO songplays(start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
     SELECT TIMESTAMP 'epoch' + e.ts/1000 * interval '1 second' as start_time,
-        e.user_id,
+        e.userId,
         e.level,
         s.song_id,
         s.artist_id,
-        e.session_id,
+        e.sessionId,
         e.location,
-        e.user_agent
+        e.userAgent
     FROM staging_events AS e
     LEFT JOIN staging_songs AS s ON
         e.song = s.title AND
@@ -158,12 +156,13 @@ songplay_table_insert = ("""
 
 user_table_insert = ("""
     INSERT INTO users(user_id, first_name, last_name, gender, level)
-    SELECT DISTINCT user_id,
-        first_name,
-        last_name,
+    SELECT DISTINCT userId,
+        firstName,
+        lastName,
         gender,
         level
-    FROM staging_events;
+    FROM staging_events
+    WHERE userId IS NOT NULL;
 """)
 
 song_table_insert = ("""
@@ -174,7 +173,8 @@ song_table_insert = ("""
         artist_id,
         year,
         duration
-    FROM staging_songs;
+    FROM staging_songs
+    WHERE song_id IS NOT NULL;
 """)
 
 artist_table_insert = ("""
@@ -184,7 +184,8 @@ artist_table_insert = ("""
         artist_location,
         artist_latitude,
         artist_longitude
-    FROM staging_songs;
+    FROM staging_songs
+    WHERE artist_id IS NOT NULL;
 """)
 
 time_table_insert = ("""
